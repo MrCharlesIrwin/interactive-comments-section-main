cf6171aebba2494286d86a0082a8a4bde5590669
Charles Irwin <charles@debian.com>
2026-01-06 21:17:17 +0300

    3

--- interactive-comments-section-main/src/App.jsx
+++ interactive-comments-section-main/src/App.jsx
@@ -11,20 +11,28 @@ function App() {
       <main>
         <article>
 
-          <section className='commentContent'>
+       <div>
 
             <img src="" alt="#" />
             <strong>ambyrobson</strong>
             <span>1 month ago</span>
             {/* <button>Reply</button> */}
-            </section>
-          
-          <aside className='commentInteraction'>
+            </div>
+
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi repudiandae assumenda ipsam pariatur ad quam similique, quaerat totam porro recusandae impedit consequatur cumque nemo architecto voluptatibus aperiam fugiat. Deserunt, aliquid!
+              Obcaecati quaerat est facilis optio dicta id ratione quos repudiandae eligendi alias cupiditate reiciendis repellat soluta doloribus distinctio sapiente vitae earum deleniti, necessitatibus officiis praesentium ex reprehenderit saepe. Voluptatum, quas?
+              Nemo aut maiores expedita enim laboriosam reprehenderit tempore. Voluptatem natus reiciendis quae dolorem recusandae cumque rerum nostrum, provident laborum laboriosam quibusdam ut officia soluta, ipsam nisi aut molestias minus placeat.
+              At aspernatur doloremque consequatur quod excepturi aut a explicabo aliquam quam! Repellendus quibusdam quia temporibus sint aspernatur distinctio ipsum amet, quae recusandae animi quo rerum nihil eaque repellat minus nostrum!
+              Doloribus itaque delectus repellendus perspiciatis officiis dolore, sint magnam fugiat iste sit necessitatibus commodi quo repellat minus temporibus, voluptate deleniti ut tempora soluta saepe assumenda. Amet earum sunt aliquid odit.
+              </p>
 
             <div className='commentCounter'></div>
+
+            <div>
+              
             <button>Delete</button>
             <button>Reply</button>
-          </aside>
+            </div>
 
         </article>
 
